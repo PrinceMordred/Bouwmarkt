aede773a6eb71d56f07d3818c7d056974e4b970e
PrinceMordred <jackrosenberg03@gmail.com>
2022-07-10 12:37:16 +0200

    Add project files.

---
+++ Bouwmarkt/Program.cs
@@ -0,0 +1,123 @@
+﻿using System;
+
+namespace Bouwmarkt // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            string[] inp  =  Console.ReadLine().Split();
+            int nKassas = int.Parse(inp[0]);
+            int nKlanten= int.Parse(inp[1]);
+            string outputMode = inp[2];
+            
+            MinHeap<Kassa> kassas = new MinHeap<Kassa>();
+
+
+
+
+            Console.ReadLine();
+        }
+        public class Kassa : IComparable<Kassa>, IHeapItem
+        {
+            public int KassaSpeed;
+            public int pos;
+            public int heapPos;
+            public Queue<long> KassaQueue;
+
+            public Kassa(int kassaSpeed, int pos)
+            {
+                KassaSpeed = kassaSpeed;
+                this.pos = pos;
+                KassaQueue = new Queue<long>();
+            }
+
+            public int CompareTo(Kassa? other)
+            {
+                
+            }
+
+            public void SetHeapIndex(int index)
+            {
+                
+            }
+        }
+        public class Event : IComparable<Event>, IHeapItem
+        {
+
+            public int CompareTo(Event other)
+            {
+                
+            }
+
+            public void SetHeapIndex(int index)
+            {
+                
+            }
+        }
+        public class MinHeap<T> where T : IComparable<T>, IHeapItem
+        {
+            List<T> A;
+
+            public MinHeap()
+            {
+                A = new List<T>();
+                A.Add(default); // make one-based list
+            }
+
+            public int length => A.Count - 1;
+
+            public int L(int i) => i << 1;
+            public int R(int i) => (i << 1) | 1;
+            public int P(int i) => i >> 1;
+            public void Heapify(int i)
+            {
+                int j = i;
+                if (L(i) <= length && A[L(i)].CompareTo(A[j]) < 0)
+                    j = L(i);
+                if (R(i) <= length && A[R(i)].CompareTo(A[j]) < 0)  
+                    j = R(i);
+                if (j != i)
+                {
+                    Swap(i, j);
+                    Heapify(j);
+                }
+            }
+            public T ExtraxtMin()
+            {
+                int n = length;
+                T res = A[1];   
+                A[1] = A[n];
+                A.RemoveAt(n);
+                Heapify(1);
+                return res;
+            }
+            public void Rootify(int i)
+            {
+                if (i == 1 || A[i].CompareTo(A[P(i)]) >= 0)
+                    return;
+                Swap(i, P(i));
+                Rootify(P(i));
+            }
+            public void Insert(T k)
+            {
+                int n = length;
+                A[++n] = k;
+                Rootify(n);
+            }
+            private void Swap(int i, int j)
+            {
+                T a = A[i];
+                A[i] = A[j];
+                A[j] = a;
+
+                A[i].SetHeapIndex(j);
+                A[j].SetHeapIndex(i);
+            }
+        }
+        public interface IHeapItem
+        {
+            void SetHeapIndex(int index);
+        }
+    }
+}
