03f24897c1eea9db1d375a06fc97369578ed8378
PrinceMordred <jackrosenberg03@gmail.com>
2022-07-12 01:56:15 +0200

    latest

--- Bouwmarkt/Program.cs
+++ Bouwmarkt/Program.cs
@@ -10,7 +10,6 @@ namespace Bouwmarkt
             int nKassas = int.Parse(inp[0]);
             int nKlanten= int.Parse(inp[1]);
             string outputMode = inp[2];
-            
             MinHeap<Kassa> kassas = new MinHeap<Kassa>();
             MinHeap<Event> events = new MinHeap<Event>();
             for (int i = 0; i < nKassas; i++)
@@ -20,16 +19,40 @@ namespace Bouwmarkt
                 inp = Console.ReadLine().Split();
                 events.Insert(new Event(int.Parse(inp[1]), long.Parse(inp[0])));
             }
+            Event e = default;
             while(events.length > 0)
             {
+                e = events.ExtraxtMin();
+                if (!(e.k is null))
+                {
+                    if (outputMode.Equals("F"))
+                        Console.WriteLine(e.tijd + ": finish " + e.k.pos);
+                    e.k.KassaQueue.Dequeue();
+                    kassas.Rootify(e.k.heapPos);
 
-
+                    if(e.k.KassaQueue.Count > 0)
+                    {
+                        if (outputMode.Equals("F"))
+                            Console.WriteLine(e.tijd + ": start " + e.k.pos);
+                        events.Insert(new Event(e.k, e.tijd + e.k.KassaSpeed * e.k.KassaQueue.First()));
+                    }
+                }
+                else
+                {
+                    Kassa k = kassas.ExtraxtMin();
+                    if (outputMode.Equals("F"))
+                        Console.WriteLine(e.tijd + ": enqueue " + k.pos);
+                    k.KassaQueue.Enqueue(e.basketSize);
+                    if(k.KassaQueue.Count == 1)
+                    {
+                        events.Insert(new Event(k, e.tijd +  k.KassaSpeed* k.KassaQueue.First()));
+                        if (outputMode.Equals("F"))
+                            Console.WriteLine(e.tijd + ": start " + k.pos);
+                    }
+                    kassas.Insert(k);
+                }
             }
-
-
-
-
-            Console.ReadLine();
+            Console.WriteLine(e.tijd + 1 + ": close " + e.k.pos);
         }
         public class Kassa : IComparable<Kassa>, IHeapItem
         {
@@ -44,20 +67,8 @@ namespace Bouwmarkt
                 this.pos = pos;
                 KassaQueue = new Queue<long>();
             }
-
-            public int CompareTo(Kassa other)
-            {
-                if (KassaQueue.Count < other.KassaQueue.Count)
-                    return -1;
-                if (KassaQueue.Count == other.KassaQueue.Count && pos < other.pos)
-                    return -1;
-                return 1;
-            }
-
-            public void SetHeapIndex(int index)
-            {
-                heapPos = index;
-            }
+            public int CompareTo(Kassa other) => KassaQueue.Count < other.KassaQueue.Count ? -1 : KassaQueue.Count == other.KassaQueue.Count && pos < other.pos ? -1 : 1;
+            public void SetHeapIndex(int index) => heapPos = index;
         }
         public class Event : IComparable<Event>, IHeapItem
         {
@@ -65,7 +76,6 @@ namespace Bouwmarkt
             public int heapPos;
             public long basketSize;
             public long tijd;
-
             public Event(Kassa k, long tijd)
             {
                 this.k = k;
@@ -76,17 +86,9 @@ namespace Bouwmarkt
                 basketSize = size;
                 this.tijd = tijd;
             }
-            public int CompareTo(Event other)
-            {
-                if (tijd < other.tijd)
-                    return -1;
-                return 1;
-            }
+            public int CompareTo(Event other) => tijd < other.tijd ? -1 : 1;
 
-            public void SetHeapIndex(int index)
-            {
-                heapPos = index;
-            }
+            public void SetHeapIndex(int index) => heapPos = index;
         }
         public class MinHeap<T> where T : IComparable<T>, IHeapItem
         {
@@ -96,9 +98,8 @@ namespace Bouwmarkt
                 A = new List<T>();
                 A.Add(default); // make one-based list
             }
-
             public int length => A.Count - 1;
-
+            // deze methods komen uit de slides
             public int L(int i) => i << 1;
             public int R(int i) => (i << 1) | 1;
             public int P(int i) => i >> 1;
@@ -126,7 +127,7 @@ namespace Bouwmarkt
             }
             public void Rootify(int i)
             {
-                if (i == 1 || A[i].CompareTo(A[P(i)]) >= 0)
+                if (i <= 1 || A[i].CompareTo(A[P(i)]) >= 0)
                     return;
                 Swap(i, P(i));
                 Rootify(P(i));
@@ -142,7 +143,6 @@ namespace Bouwmarkt
                 T a = A[i];
                 A[i] = A[j];
                 A[j] = a;
-
                 A[i].SetHeapIndex(j);
                 A[j].SetHeapIndex(i);
             }
